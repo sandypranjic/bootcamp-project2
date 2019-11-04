8a1e54eb950f102f42b348f71cddebc1dc9e7700
Sandy Pranjic <sandypranjic@gmail.com>
2019-11-03 19:06:04 -0500

    added jquery on blog page so that users can add comments to the blog

--- javascript.js
+++ javascript.js
@@ -1,5 +1,33 @@
+// take the value of the textarea and store it in a variable
+// create a function that takes the value of the textarea and appends it underneath the other comments
+// when the submit button is pressed, trigger the function that adds a new comment
+
+
+
+    const comment = {};
+
+    comment.commentText = $("textarea").val();
+    comment.commenterName = $('input[type="text"]').val();
+
+    comment.commentHtml = `
+        <div class="comment">
+            <div class="userImg">
+                <img src="https://placebear.com/92/89">
+            </div>
+            <div class="userComment">
+                <span class="postDate">Tuesday, November 21st, 2019 by ${comment.commenterName}</span>
+                <p>${comment.commentText}</p>
+            </div>
+        </div>
+    `;
+
+    comment.init = function() {
+        $("form").on("submit", function(event) {
+            event.preventDefault();
+            $(".commentsContainer").append(comment.commentHtml);
+        });
+    };
+
 $(function() {
-
-// jquery here
-
+    comment.init();
 });
